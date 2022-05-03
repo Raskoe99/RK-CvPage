f54a860aafc3a920d0bcb2bfe6e426be173111a8
Raskoe99 <r.koefoed@hotmail.com>
2022-05-03 11:45:29 +0200

    Updating of logos, frontpage, and general css

---
+++ RK-CvPage/src/components/Topbar.svelte
@@ -0,0 +1,16 @@
+<script>
+    
+</script>
+
+<div class="header">
+    <img class="logo" src="./img/logo.png" alt="Logo"/>
+    <div class="container">
+        <nav class="nav">
+            <ul>
+                <li><a href="../introduction" id="introduction">Intro & datatypes</a></li>
+                <li><a href="../firstServer" id="firstServer">First server</a></li>
+                <li><a href="../servingHTML" id="servingHTML">Serving HTML</a></li>
+            </ul>
+        </nav>
+    </div>
+</div>
---
+++ RK-CvPage/src/pages/Home.svelte
@@ -0,0 +1,26 @@
+<script>
+	import Topbar from "../components/Topbar.svelte";
+</script>
+
+<Topbar/>
+<main>
+	<h1>Hello and welcome!</h1>
+	<div class="bodyText">
+        Oh no, this must look incredibly ugly
+    </div>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
